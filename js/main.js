879dd0657d22c01496b411ffcbb6952be86f82bf
Charlotte Rushen <charlotterushen@hotmail.com>
2019-04-09 14:48:03 +0100

    Adding key of D

--- js/main.js
+++ js/main.js
@@ -9,4 +9,9 @@ var cSharpMin = ['C♯m', 'D♯dim', 'E', 'F♯m', 'G♯m', 'A', 'B'];
 // Key: D 
 var dMaj = ['D', 'Em', 'F♯m', 'G', 'A', 'Bm', 'C♯dim'];
 var dMin = ['Dm', 'Edim', 'F', 'Gm', 'Am', 'B♭', 'C'];
+var dFlatMaj = ['D♭', 'E♭m', 'Fm', 'G♭', 'A♭', 'B♭m', 'Cdim'];
+var dSharpMaj = ['D♯', 'E♯m', 'F♯♯m', 'G♯', 'A♯', 'B♯m', 'C♯♯dim'];
+var dFlatMin = ['D♭m', 'E♭dim', 'F♭', 'G♭m', 'A♭m', 'B♭♭', 'C♭'];
+var dSharpMin = ['D♯m', 'E♯dim', 'F♯', 'G♯m', 'A♯m', 'B', 'C♯'];
+
 
