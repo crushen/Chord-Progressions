e1daf9d58b8feeba3e3ace05a91b7315c2a81ffa
Charlotte Rushen <charlotterushen@hotmail.com>
2019-04-09 14:29:34 +0100

    First Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Each key has an array of all chords found in key ♭♯
+// Key: C
+var cMaj = ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bdim'];
+var cMin = ['Cm', 'Ddim', 'E♭', 'Fm', 'Gm', 'A♭', 'B♭'];
+var cFlatMaj = ['C♭', 'D♭m', 'E♭m', 'F♭', 'G♭', 'A♭m', 'B♭dim'];
+var cSharpMaj = ['C♯', 'D♯m', 'E♯m', 'F♯', 'G♯', 'A♯m', 'B♯dim'];
+var cFlatMin = ['C♭m', 'D♭dim', 'E♭♭', 'F♭m', 'G♭m', 'A♭♭', 'B♭♭'];
+var cSharpMin = ['C♯m', 'D♯dim', 'E', 'F♯m', 'G♯m', 'A', 'B'];
+// Key: D 
+var dMaj = ['D', 'Em', 'F♯m', 'G', 'A', 'Bm', 'C♯dim'];
+var dMin = ['Dm', 'Edim', 'F', 'Gm', 'Am', 'B♭', 'C'];
+
